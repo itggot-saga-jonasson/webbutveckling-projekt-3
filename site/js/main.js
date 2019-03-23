3febea0c47afbc57409b3f4903d7f763914e2f97
itggot-saga-jonasson <saga.jonasson@elev.ga.ntig.se>
2019-03-24 00:49:37 +0100

    created some more asteroid images and created the start of a functional menu through the stupidest way imaginable

--- site/js/main.js
+++ site/js/main.js
@@ -15,3 +15,12 @@ function bringBackMenu(){
         y.classList.remove("tadaa");
     }
 }
+
+
+function recommended(){
+    var recommendContainment = '<div class="product_box"> <a href=""> <div class="product_img" id="asteroid_3"></div></a><div class="product_info"><h1>Mapus 7VU</h1><ul><p><u>Radius: 200m</u></p><p>40% gold</p><p>10% copper</p><p>30% iron</p></ul><div class="price">¤200,000</div><i class="material-icons">shopping_cart</i></div></div>';
+
+    document.getElementsByClassName("product_area")[0].innerHTML = recommendContainment
+    console.log = "hellu"
+}
+// if it's stupid, but it works, it ain't stupid
