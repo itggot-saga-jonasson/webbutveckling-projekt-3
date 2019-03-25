03f7d4388162df4e0b3478939bc7e9af1b1efbd8
itggot-saga-jonasson <saga.jonasson@elev.ga.ntig.se>
2019-03-25 10:13:40 +0100

    Tried to fix the menu navigation thing and failed

--- site/js/main.js
+++ site/js/main.js
@@ -18,11 +18,16 @@ function bringBackMenu(){
 
 
 function recommended(){
-    var recommendContainment = [
-        '<div class="product_box"> <a href=""> <div class="product_img" id="asteroid_3"></div></a><div class="product_info"><h1>Mapus 7VU</h1><ul><p><u>Radius: 200m</u></p><p>40% gold</p><p>10% copper</p><p>30% iron</p></ul><div class="price">¤200,000</div><i class="material-icons">shopping_cart</i></div></div>'
-    ]
 
-    document.getElementsByClassName("product_area")[0].innerHTML = recommendContainment
+    while(document.querySelectorAll(".visible").length > 0){
+        document.getElementsByClassName(".visible")[0].classList.remove("visible")
+    }
+
+    document.getElementById("asteroid_1").className += " visible"
+    document.getElementById("asteroid_2").className += " visible"
+    document.getElementById("asteroid_3").className += " visible"
+    document.getElementById("comet_1").className += " visible"
+
     document.getElementsByClassName("info_box")[0].innerHTML = "<p>Recommended</p>"
 }
 // if it's stupid but it works, it ain't stupid
